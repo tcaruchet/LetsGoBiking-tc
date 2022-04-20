140cfb0a030efb039d3645aa8402380611c0e25d
Thomas Caruchet <thomas.caruchet@etu.univ-cotedazur.fr>
2022-04-20 16:33:37 +0200

    tyuiop

--- LetsGoBiking-tc.Web/server.py
+++ LetsGoBiking-tc.Web/server.py
@@ -4,5 +4,5 @@ PORT = 8009
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-	print("Server launched at http://localhost:" + PORT)
+	print("Server launched at http://localhost:" + str(PORT))
 	httpd.serve_forever()
